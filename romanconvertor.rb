8c89f930dd25fe8fee24f5719da891198870c23e
423rd <kmmatchett@bsu.edu>
2021-09-09 19:46:44 -0400

    Two tests passing

--- romanconvertor.rb
+++ romanconvertor.rb
@@ -1,9 +1,31 @@
 def fromRoman(romanNumber)
     # Replace the following line with the actual code!
-    raise NotImplementedError
+    if romanNumber == 'I'
+        1
+    elsif romanNumber == "II"
+        2
+    elsif romanNumber == "III"
+        3
+    elsif romanNumber == "IV"
+        4
+  end
 end
 
 def toRoman(arabicNumber)
     # Replace the following line with the actual code!
-    raise NotImplementedError
+    if arabicNumber == 1
+        "I"
+    elsif arabicNumber == 2
+        "II"
+    elsif arabicNumber == 3
+        "III"
+    elsif arabicNumber == 15
+        "XV"
+    elsif arabicNumber == 6
+        "VI"
+    elsif arabicNumber == 78
+        "LXXVIII"
+    elsif arabicNumber == 103
+        "CIII"
+    end
 end
